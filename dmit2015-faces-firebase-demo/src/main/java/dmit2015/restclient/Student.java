13a2ab185302fd7843062bb2730e11885594e620
Perhapsjustaname <mkhawaja1@nait.ca>
2023-05-17 16:16:23 -0600

    created project

---
+++ dmit2015-faces-firebase-demo/src/main/java/dmit2015/restclient/Student.java
@@ -0,0 +1,19 @@
+package dmit2015.restclient;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Getter @Setter
+@NoArgsConstructor
+public class Student {
+
+
+    private String firstName;
+
+
+
+    private String lastName;
+
+
+}
